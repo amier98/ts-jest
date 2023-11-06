a6751e51e84d920aee2d9846adebde6682436a66
Amier Ali <amier_1998@hotmail.com>
2023-11-06 22:27:10 +0000

    feat: Implement convertRoman function

--- src/calc.ts
+++ src/calc.ts
@@ -1,3 +1,42 @@
-export function add(x: number, y: number): number {
-  return x + y;
+//step one: convert number to roman numeral
+
+// roman numerals - 1 to 3 are labeled I
+// from 4 onwards, they use V - so 4 is IV
+// so if number is greater than 5, you add I
+
+//while loops maybe
+
+export function convertRoman(nums: number) {
+  let roman = "";
+
+  const obj = {
+    M: 1000,
+    DM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  for (let every in obj) {
+    while (nums >= obj[every]) {
+      roman += every;
+      nums -= obj[every];
+    }
+  }
+  // if (nums === 9) {
+  //   return "IX";
+  // }
+  // if (nums === 4) {
+  //   return "IV";
+  // }
+
+  return roman;
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,40 @@
-import { add } from "../src/calc";
+import { convertRoman } from "../src/calc";
 
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(add(10, 5)).toBe(15);
+describe("tests for romanConvert function", () => {
+  it("should return converted numbers to roman numerals", () => {
+    expect(convertRoman(3)).toEqual("III");
   });
-  it("should return 5 for add(2,3)", () => {
-    expect(add(2, 3)).toBe(5);
+  it("return roman numeral of 4 ", () => {
+    expect(convertRoman(4)).toEqual("IV");
+  });
+  it("return converted roman numeral to greater than 5", () => {
+    expect(convertRoman(5)).toEqual("V");
+  });
+  it("return converted roman numeral is eight", () => {
+    expect(convertRoman(8)).toEqual("VIII");
+  });
+  it("return roman numeral of 10 ", () => {
+    expect(convertRoman(10)).toEqual("X");
+  });
+  it("return roman numeral of 24 ", () => {
+    expect(convertRoman(24)).toEqual("XXIV");
+  });
+  it("return roman numeral of 23 ", () => {
+    expect(convertRoman(23)).toEqual("XXIII");
+  });
+  it("return roman numeral of 50 ", () => {
+    expect(convertRoman(50)).toEqual("L");
+  });
+  it("return roman numeral of 60 ", () => {
+    expect(convertRoman(60)).toEqual("LX");
+  });
+  it("return roman numeral of 239 ", () => {
+    expect(convertRoman(239)).toEqual("CCXXXIX");
+  });
+  it("return roman numeral of 4 ", () => {
+    expect(convertRoman(4)).toEqual("IV");
+  });
+  it("return roman numeral of 9 ", () => {
+    expect(convertRoman(9)).toEqual("IX");
   });
 });
